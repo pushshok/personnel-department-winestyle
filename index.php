ed209ad521bd51c5399d468dd095903364e0f221
pushshok <pushshok@gmail.com>
2019-09-03 21:54:00 +0300

    Настроил вывод данных рабочих из модели в индексный файл.

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?= rand(30000, 70000); ?>
+<?php
+require_once "connect.php";
+require_once 'models/select-workers.php';
+$workers = getWorkers();
+?>
+
+<?php foreach ($workers as $worker): ?>
+
+    <div class="row">
+        <div class="col-md-2">
+            <?= $worker['id'] . "<br>"; ?>
+            <?= $worker['name'] . "<br>"; ?>
+            <?= $worker['last_name'] . "<br>"; ?>
+            <?= ucfirst(getPosition($worker['position_id'])) . "<br>"; ?>
+        </div>
+    </div>
+<?php endforeach; ?>
--- models/select-workers.php
+++ models/select-workers.php
@@ -1,1 +1,19 @@
 <?php
+require_once "connect.php";
+
+
+function getWorkers()
+{
+    $connect = DB::getConnect();
+    $workers = $connect->query("SELECT * FROM `personnel_department`.`workers`;")->fetchAll(PDO::FETCH_ASSOC);
+
+    return $workers;
+}
+
+function getPosition($id)
+{
+    $connect = DB::getConnect();
+    $position = $connect->query("SELECT `position` FROM `personnel_department`.`professions` WHERE `id`= '".$id."';")->fetch(PDO::FETCH_ASSOC);
+
+    return $position['position'];
+}
